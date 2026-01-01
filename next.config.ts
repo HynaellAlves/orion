27f61c21c987261dcc7578ad1686a655b5cb3412
Hynaell Alves <hynaell@gmail.com>
2026-01-01 10:37:02 -0300

    Feat: Adding configurations in the configurations file to activate the domains images

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* Confugurações do Next.js */
   reactCompiler: true,
+  /* Configurando provedores de Imagens */
+  // ----- Adicionar o Prismic como provedor de imagens -----
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'images.prismic.io' },
+      { protocol: 'https', hostname: 'prismic.io' },
+    ]
+  },
 };
 
 export default nextConfig;
