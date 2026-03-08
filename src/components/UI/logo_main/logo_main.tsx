5e05d9f1dbcd78e5da1b9f89c904f864d8ec4f82
Hynaell Alves <hynaell@gmail.com>
2026-03-08 08:45:02 -0300

    Fix: Removing files of Muito+

--- src/components/UI/logo_main/logo_main.tsx
+++ src/components/UI/logo_main/logo_main.tsx
@@ -3,7 +3,7 @@ import styles from "./logo_main.module.css";
 export default function logo_main(){
 return(
     <div id={styles.logo_main} className={styles.logo_main}>
-        <img src="/muito-educacao-profissional.png" alt="Logo Muito Educação Profissional" />
+        <img src="/muito-educacao-profissional.png" alt="Logo da empresa" />
     </div>
 )
 }
